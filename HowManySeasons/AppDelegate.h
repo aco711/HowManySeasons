489d70251cfd537b45bfe4dcc9483700e19f33e8
aco711 <alexcohen711@gmail.com>
2015-08-23 12:26:11 -0400

    Initial Commit

---
+++ HowManySeasons/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HowManySeasons
+//
+//  Created by Alex Cohen on 8/23/15.
+//  Copyright (c) 2015 Alex Cohen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HowManySeasons/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HowManySeasons
+//
+//  Created by Alex Cohen on 8/23/15.
+//  Copyright (c) 2015 Alex Cohen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
